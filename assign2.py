39796564cecb7e222ef5112b1b8a643b392ffac9
Aalok Tiwari <85796262+aaloktiwariofficial@users.noreply.github.com>
2022-08-24 18:37:24 +0530

    Create assign2.py

---
+++ assign2.py
@@ -0,0 +1,9 @@
+aTuple=(["apple", "cat",25, False])
+print(aTuple)
+aTuple.append("dog")
+aTuple.remove("cat")
+print(aTuple)
+
+aDictionary={"Brand":["Dell",False],"color":"Voilet","Model":2098}
+print(aDictionary)
+print(aDictionary["Brand"])
